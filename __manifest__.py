4d0043ff6068e051649da7d547ba3e7b2dca52bf
Hoàng Bùi <buituhoang@gmail.com>
2021-04-16 10:00:10 +0700

    First init

---
+++ __manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name' : 'Weather Forecast',
+    'version' : '0.1',
+    'summary': 'Weather Forecast',
+    'sequence': -10,
+    'description': """Weather Forecast""",
+    'category': 'Productivity',
+    'website': 'https://www.abcxyz.com/',
+    'license': 'LGPL-3',
+    'depends': ['website', 'base', 'web'],
+    'data': [
+
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+}
